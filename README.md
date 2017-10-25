3fbc592e9133da586cfac3b7d815266595c4c2e5
Tam Nguyen <ntamvl@gmail.com>
2017-10-25 17:41:29 +0700

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Simple BlockChain
 Simple BlockChain implement by Go Lang
 
+## How to build
+```
+cd && git clone https://github.com/ntamvl/simple_blockchain.git
+cd simple_blockchain 
+go get
+go build
+```
+
+## How to run 
+```
+./simple_blockchain
+```
+
+Then see result the same below: 
+```
+
+```
--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 func main() {
 	bc := NewBlockchain()
 
-	bc.AddBlock("Send 1 BTC to Ivan")
-	bc.AddBlock("Send 2 more BTC to Ivan")
+	bc.AddBlock("Send 1 BTC to Tam")
+	bc.AddBlock("Send 2 more BTC to Tam")
 
 	for _, block := range bc.blocks {
 		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
